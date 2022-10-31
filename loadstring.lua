40ad7021f31ebf3ec34121151b61ed48a24b10ae
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-10-31 23:45:43 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,7 +3,6 @@
       Supported Games:
                         Build A Boat For Treasure           https://www.roblox.com/games/537413528/Build-A-Boat-For-Treasure
                         Tapping Simulator                   https://www.roblox.com/games/9498006165/UPD18-Tapping-Simulator
-
 ]]---
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Main.lua"))()
