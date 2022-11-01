c6f7accbc150b8272a6cb95ce5dd65425e8b1af7
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-11-01 03:55:44 +0100

    Update Tapping Simulator.lua

--- Tapping Simulator.lua
+++ Tapping Simulator.lua
@@ -1,10 +1,70 @@
+for i, v in next, game.CoreGui:GetChildren() do
+    if v.Name:find("Vynixius UI Library") then
+        v:Destroy()
+    end
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Vynixius/Source.lua"))()
 local Window = Library:AddWindow({
-	title = {"Vynixius", "UI Library"},
+	title = {"Tapping Simulator", "No.8#9194"},
 	theme = {
 		Accent = Color3.fromRGB(0, 255, 0)
 	},
 	key = Enum.KeyCode.RightControl,
 	default = true
 })
-local Main = Window:AddTab("Main", {default = false})
+-- Main
+local MainTab = Window:AddTab("Main", {default = false})
+local MainshitSection = MainTab:AddSection("Main shit", {default = false})
+local looping = false
+local Toggle = MainshitSection:AddToggle("AutoClicker", {flag = "Toggle_Flag", default = false}, function(bool)
+    looping = bool 
+    
+    task.spawn(function() 
+        while looping == true do
+            game:GetService("ReplicatedStorage").Events.Tap:FireServer(1)
+            wait()
+        end
+    end)
+end)
+local Toggle = MainshitSection:AddToggle("Auto Claim Random Rewards", {flag = "Toggle_Flag", default = false}, function(bool)
+    looping = bool 
+    
+    task.spawn(function() 
+        while looping == true do
+            game:GetService("ReplicatedStorage").Events.ClaimRandomReward:FireServer()
+            wait(10)
+        end
+    end)
+end)
+
+-- Localplayer
+local Local_Player = Window:AddTab("Local player", {default = false})
+local Local_PlayerSection = Local_Player:AddSection("Basic Shit", {default = false})
+local previousWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, 16, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
+	if bool then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+	end
+	if bool == false then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = previousWalkSpeed
+	end
+end)
+local previousJumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, 50, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val2, bool2)
+	if bool2 then
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = val2
+	end
+	if bool2 == false then
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = previousJumpPower
+	end
+end)
+local previousGravity = game.Workspace.Gravity
+local Slider = Local_PlayerSection:AddSlider("Gravity", 0, 300, previousGravity, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val3, bool3)
+	if bool3 then
+		game.Workspace.Gravity = val3
+	end
+	if bool3 == false then
+		game.Workspace.Gravity = previousGravity
+	end
+end)
