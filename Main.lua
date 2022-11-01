f10e1c9d3bd8ba21de79265d28d0708ece380b89
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-11-01 05:08:08 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20A%20Boat%20For%20Treasure"))()
 elseif game.PlaceId == 9498006165 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Tapping%20Simulator.lua"))()
+elseif game.PlaceId == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/MM2.lua"))()
 end
