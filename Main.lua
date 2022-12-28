66373f8ad119e4f4aabfcf13f6651e01b3a07c7e
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-12-28 23:46:19 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,6 +4,6 @@ elseif game.PlaceId == 9498006165 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Tapping%20Simulator.lua"))()
 elseif game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/MM2.lua"))()
-elseif game.PlaceID == 5149673492 then
+elseif game.PlaceId == 5149673492 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20to%20survive%20the%20Bombs!"))()
 end
