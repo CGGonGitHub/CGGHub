3edbab26e494f4823115bd611b55b8bbb770ce43
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-12-31 02:44:48 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,9 +1,16 @@
-if game.PlaceId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20A%20Boat%20For%20Treasure.lua"))()
-elseif game.PlaceId == 9498006165 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Tapping%20Simulator.lua"))()
-elseif game.PlaceId == 142823291 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/MM2.lua"))()
-elseif game.PlaceId == 5149673492 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20to%20survive%20the%20Bombs!.lua"))()
+local Games = {
+	[537413528] = "https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20A%20Boat%20For%20Treasure.lua", -- Build A Boat For Treasure
+	[5149673492] = "https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Build%20to%20survive%20the%20Bombs!.lua", -- Build To Survive The Bombs!
+	[9498006165] = "https://raw.githubusercontent.com/CGGonGitHub/Loader/main/Tapping%20Simulator.lua", -- Tapping Simulator
+	[142823291] = "https://raw.githubusercontent.com/CGGonGitHub/Loader/main/MM2.lua" -- Murder Mystery 2
+}
+
+if Games[game.PlaceId] then
+	loadstring(game:HttpGet(Games[game.PlaceId]))()
+else
+	game.StarterGui:SetCore("SendNotification", {
+	    Title = "CGG says";
+	    Text = "Game not supported, here is a universal one";
+	    Duration = 10;
+    })
 end
