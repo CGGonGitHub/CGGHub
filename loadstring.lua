c9c38e3b6dc3f420ca5393519455dadc32f971d4
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-12-31 02:48:05 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,6 @@
 --[[ 
-                                       Made by No.8#9194
+                                           Made by No.8#9194 // CGG // CoolGermanGuy
+                                                                                                             Unprotected by nothing v2
       Supported Games:
                         Build A Boat For Treasure           https://www.roblox.com/games/537413528/Build-A-Boat-For-Treasure
                         Tapping Simulator                   https://www.roblox.com/games/9498006165/UPD18-Tapping-Simulator
