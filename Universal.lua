17b9f680c5996f7a123458abbc0e8f1886fa475b
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-12-31 03:12:54 +0100

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -21,3 +21,84 @@ local Window = Library:AddWindow({
 	key = Enum.KeyCode.RightControl,
 	default = true
 })
+-- Localplayer
+local Local_Player = Window:AddTab("Local player", {default = false})
+local Local_PlayerSection = Local_Player:AddSection("Basic Shit", {default = false})
+local previousWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, 16, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
+	if bool then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+	end
+	if bool == false then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = previousWalkSpeed
+	end
+end)
+local previousJumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, 50, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val2, bool2)
+	if bool2 then
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = val2
+	end
+	if bool2 == false then
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = previousJumpPower
+	end
+end)
+local previousGravity = game.Workspace.Gravity
+local Slider = Local_PlayerSection:AddSlider("Gravity", 0, 300, previousGravity, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val3, bool3)
+	if bool3 then
+		game.Workspace.Gravity = val3
+	end
+	if bool3 == false then
+		game.Workspace.Gravity = previousGravity
+	end
+end)
+
+-- Teleports
+local TeleportTab = Window:AddTab("Teleports", {default = false})
+local TeleportSection = TeleportTab:AddSection("Players", {default = false})
+function getAllPlayers()
+    local playertable =  {}
+    for i, v in next, game.Players:GetPlayers() do
+        table.insert(playertable, v.Name)
+    end
+    return playertable
+end
+function Teleport(CFrame)
+    game.Players.LocalPlayer.Character.PrimaryPart:PivotTo(CFrame)
+end
+local Dropdown = TeleportSection:AddDropdown("Players", getAllPlayers(), {default = ""}, function(selected)
+    
+    x = Instance.new("Part")
+    
+    x.Shape = Enum.PartType.Cylinder
+    x.Size = Vector3.new(1000,0.5,0.5)
+    x.Material = Enum.Material.Neon
+    x.Color = Color3.fromRGB(0, 225, 255)
+    x.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    x.Rotation = Vector3.new(0,0,90)
+    x.CanCollide = false
+    x.Anchored = true
+    x.Parent = game.Workspace
+    local TS = game:GetService("TweenService")
+    local Anim = TS:Create(x, TweenInfo.new(1), {Size=Vector3.new(1000,5,5)})
+    Anim:Play()
+    task.wait(1)
+    x:Destroy()
+    Teleport(game.Players[selected].Character.HumanoidRootPart.CFrame)
+    x = Instance.new("Part")
+    
+    x.Shape = Enum.PartType.Cylinder
+    x.Size = Vector3.new(1000,5,5)
+    x.Material = Enum.Material.Neon
+    x.Color = Color3.fromRGB(0, 225, 255)
+    x.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    x.Rotation = Vector3.new(0,0,90)
+    x.CanCollide = false
+    x.Parent = game.Workspace
+    x.Anchored = true
+    local TS = game:GetService("TweenService")
+    local Anim = TS:Create(x, TweenInfo.new(0.5), {Size=Vector3.new(1000,0.5,0.5)})
+    Anim:Play()
+    Anim.Completed:Connect(function()
+        x:Destroy()
+    end)
+end)
