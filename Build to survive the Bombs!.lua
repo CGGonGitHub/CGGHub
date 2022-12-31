9cf0d10eaea28cddd9965cfd6fe06bd4e5c9aa9f
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2022-12-31 16:27:42 +0100

    Update Build to survive the Bombs!.lua

--- Build to survive the Bombs!.lua
+++ Build to survive the Bombs!.lua
@@ -156,7 +156,7 @@ end)
 local Local_Player = Window:AddTab("Local player", {default = false})
 local Local_PlayerSection = Local_Player:AddSection("Basic Shit", {default = false})
 local previousWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
-local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, 16, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
+local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
 	if bool then
 		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
 	end
@@ -165,7 +165,7 @@ local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, 16, {
 	end
 end)
 local previousJumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
-local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, 50, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val2, bool2)
+local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, game.Players.LocalPlayer.Character.Humanoid.JumpPower, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val2, bool2)
 	if bool2 then
 		game.Players.LocalPlayer.Character.Humanoid.JumpPower = val2
 	end
@@ -174,7 +174,7 @@ local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, 50, 
 	end
 end)
 local previousGravity = game.Workspace.Gravity
-local Slider = Local_PlayerSection:AddSlider("Gravity", 0, 300, previousGravity, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val3, bool3)
+local Slider = Local_PlayerSection:AddSlider("Gravity", 1, 500, game.Workspace.Gravity, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val3, bool3)
 	if bool3 then
 		game.Workspace.Gravity = val3
 	end
@@ -182,6 +182,30 @@ local Slider = Local_PlayerSection:AddSlider("Gravity", 0, 300, previousGravity,
 		game.Workspace.Gravity = previousGravity
 	end
 end)
+local NoClipToggle = Local_PlayerSection:AddToggle("Noclip", {flag = "Toggle_Flag", default = false}, function(bool4)
+    local Players = game:GetService("Players")
+    local Workspace = game:GetService("Workspace")
+    local Plr = Players.LocalPlayer
+    
+    if bool4 == true then
+        Clipon = true
+        Stepped = game:GetService("RunService").Stepped:Connect(function()
+            if not Clipon == false then
+                for a, b in pairs(Workspace:GetChildren()) do
+                if b.Name == Plr.Name then
+                for i, v in pairs(Workspace[Plr.Name]:GetChildren()) do
+                if v:IsA("BasePart") then
+                v.CanCollide = false
+                end end end end
+            else
+                Stepped:Disconnect()
+            end
+        end)
+    elseif bool4 == false then
+        Clipon = false
+    end
+end)
+
 
 
 
