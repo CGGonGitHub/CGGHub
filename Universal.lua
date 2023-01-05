bcbf0a7213fe0d5fe26a11bda1578b270f08941f
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-01-06 00:32:57 +0100

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -34,24 +34,27 @@ _G.spam = "hello, this is the spammer from CGG's ui"
 _G.spamenabled = true
 _G.loopteleportenabled = true
 ESPbool = false
+lp = game:GetService("Players").LocalPlayer
 VU = game:GetService("VirtualUser")
+Mouses = lp:GetMouse()
+cmdm = Mouses
 -- Localplayer
 local Local_Player = Window:AddTab("Local player", {default = false})
 local Local_PlayerSection = Local_Player:AddSection("Basic Shit", {default = false})
-local previousWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
-local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
+local previousWalkSpeed = lp.Character.Humanoid.WalkSpeed
+local WalkSpeedSlider = Local_PlayerSection:AddSlider("Walkspeed", 1, 300, lp.Character.Humanoid.WalkSpeed, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
 	if bool then
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+		lp.Character.Humanoid.WalkSpeed = val
     elseif not bool then
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = previousWalkSpeed
+		lp.Character.Humanoid.WalkSpeed = previousWalkSpeed
 	end
 end)
-local previousJumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
-local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, game.Players.LocalPlayer.Character.Humanoid.JumpPower, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
+local previousJumpPower = lp.Character.Humanoid.JumpPower
+local JumpPowerSlider = Local_PlayerSection:AddSlider("JumpPower", 1, 3000, lp.Character.Humanoid.JumpPower, {toggleable = true, default = false, flag = "Slider_Flag", fireontoggle = true, fireondrag = true, rounded = true}, function(val, bool)
 	if bool then
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = val
+		lp.Character.Humanoid.JumpPower = val
     elseif not bool then
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = previousJumpPower
+		lp.Character.Humanoid.JumpPower = previousJumpPower
 	end
 end)
 local previousGravity = game.Workspace.Gravity
@@ -94,11 +97,132 @@ local NoClipToggle = Local_PlayerSection:AddToggle("Noclip", {flag = "Toggle_Fla
         Clipon = false
     end
 end)
-local Toggle = Local_PlayerSection:AddToggle("Anchored", {flag = "Toggle_Flag", default = false}, function(bool)
-	if bool then 
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+local lp = game.Players.LocalPlayer
+Mouses = lp:GetMouse()
+mouse = Mouses
+
+
+function sFLY(vfly)
+	FLYING = false
+	speedofthefly = 1
+	speedofthevfly = 1
+	while not lp or not lp.Character or not lp.Character:FindFirstChild('HumanoidRootPart') or not lp.Character:FindFirstChild('Humanoid') or not cmdm do
+		 wait()
+	end 
+	local T = lp.Character.HumanoidRootPart
+	local CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+	local lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+	local SPEED = 0
+	local function FLY()
+		FLYING = true
+		local BG = Instance.new('BodyGyro', T)
+		local BV = Instance.new('BodyVelocity', T)
+		BG.P = 9e4
+		BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+		BG.cframe = T.CFrame
+		BV.velocity = Vector3.new(0, 0, 0)
+		BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+		spawn(function()
+			while FLYING do
+				if not vfly then
+					lp.Character:FindFirstChild("Humanoid").PlatformStand = true
+				end
+				if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0 then
+					SPEED = 50
+				elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0) and SPEED ~= 0 then
+					SPEED = 0
+				end
+				if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 or (CONTROL.Q + CONTROL.E) ~= 0 then
+					BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+					lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+				elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and (CONTROL.Q + CONTROL.E) == 0 and SPEED ~= 0 then
+					BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+				else
+					BV.velocity = Vector3.new(0, 0, 0)
+				end
+				BG.cframe = workspace.CurrentCamera.CoordinateFrame
+				wait()
+			end
+			CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+			lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+			SPEED = 0
+			BG:destroy()
+			BV:destroy()
+			lp.Character.Humanoid.PlatformStand = false
+		end)
+	end
+	cmdm.KeyDown:connect(function(KEY)
+		if KEY:lower() == 'w' then
+			if vfly then
+				CONTROL.F = speedofthevfly
+			else
+				CONTROL.F = speedofthefly
+			end
+		elseif KEY:lower() == 's' then
+			if vfly then
+				CONTROL.B = - speedofthevfly
+			else
+				CONTROL.B = - speedofthefly
+			end
+		elseif KEY:lower() == 'a' then
+			if vfly then
+				CONTROL.L = - speedofthevfly
+			else
+				CONTROL.L = - speedofthefly
+			end
+		elseif KEY:lower() == 'd' then
+			if vfly then
+				CONTROL.R = speedofthevfly
+			else
+				CONTROL.R = speedofthefly
+			end
+		elseif KEY:lower() == 'y' then
+			if vfly then
+				CONTROL.Q = speedofthevfly*2
+			else
+				CONTROL.Q = speedofthefly*2
+			end
+		elseif KEY:lower() == 't' then
+			if vfly then
+				CONTROL.E = -speedofthevfly*2
+			else
+				CONTROL.E = -speedofthefly*2
+			end
+		end
+	end)
+	cmdm.KeyUp:connect(function(KEY)
+		if KEY:lower() == 'w' then
+			CONTROL.F = 0
+		elseif KEY:lower() == 's' then
+			CONTROL.B = 0
+		elseif KEY:lower() == 'a' then
+			CONTROL.L = 0
+		elseif KEY:lower() == 'd' then
+			CONTROL.R = 0
+		elseif KEY:lower() == 'y' then
+			CONTROL.Q = 0
+		elseif KEY:lower() == 't' then
+			CONTROL.E = 0
+		end
+	end)
+	FLY()
+end
+
+local FlyToggle = Local_PlayerSection:AddToggle("Fly", {flag = "Toggle_Flag", default = false}, function(bool)
+	if bool then
+        print("fly")
+        sFLY(vfly)
     elseif not bool then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+        FLYING = false
+        lp.Character.Humanoid.PlatformStand = false
+    end
+end)
+
+local AnchoredToggle = Local_PlayerSection:AddToggle("Anchored", {flag = "Toggle_Flag", default = false}, function(bool)
+	if bool then 
+        lp.Character.HumanoidRootPart.Anchored = true
+    elseif not bool then
+        lp.Character.HumanoidRootPart.Anchored = false
     end
 end)
 
@@ -113,7 +237,7 @@ function getAllPlayers()
     return playertable
 end
 function Teleport(CFrame)
-    game.Players.LocalPlayer.Character.PrimaryPart:PivotTo(CFrame)
+    lp.Character.PrimaryPart:PivotTo(CFrame)
 end
 local Dropdown = TeleportSection:AddDropdown("Player to teleport to", getAllPlayers(), {default = ""}, function(selected)
     x = Instance.new("Part")
@@ -121,7 +245,7 @@ local Dropdown = TeleportSection:AddDropdown("Player to teleport to", getAllPlay
     x.Size = Vector3.new(1000,0.5,0.5)
     x.Material = Enum.Material.Neon
     x.Color = Color3.fromRGB(0, 225, 255)
-    x.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    x.CFrame = lp.Character.HumanoidRootPart.CFrame
     x.Rotation = Vector3.new(0,0,90)
     x.CanCollide = false
     x.Anchored = true
@@ -138,7 +262,7 @@ local Dropdown = TeleportSection:AddDropdown("Player to teleport to", getAllPlay
     x.Size = Vector3.new(1000,5,5)
     x.Material = Enum.Material.Neon
     x.Color = Color3.fromRGB(0, 225, 255)
-    x.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    x.CFrame = lp.Character.HumanoidRootPart.CFrame
     x.Rotation = Vector3.new(0,0,90)
     x.CanCollide = false
     x.Parent = game.Workspace
@@ -169,7 +293,7 @@ end)
 local function loopteleport()
     _G.loopteleportenabled = not _G.loopteleportenabled
     while _G.loopteleportenabled do
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[xyz].Character.HumanoidRootPart.CFrame
+        lp.Character.HumanoidRootPart.CFrame = game.Players[xyz].Character.HumanoidRootPart.CFrame
         task.wait()
     end
 end
@@ -205,7 +329,7 @@ end
 local afkConnection
 local Toggle = AntiafkSection:AddToggle("no more idle 20 min kick >:(", {flag = "Toggle_Flag", default = false}, function(bool)
 if bool then
-    afkConnection = game.Players.LocalPlayer.Idled:Connect(antiafk)
+    afkConnection = lp.Idled:Connect(antiafk)
 else 
     afkConnection:Disconnect()
 end
@@ -331,7 +455,7 @@ local JoinSection = MiscTab:AddSection("Rejoin & Co", {default = false})
 local Button = JoinSection:AddButton("Rejoin", function()
 	JobId = game.JobId
     PlaceId = game.PlaceId
-    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceId, JobId, game.Players.LocalPlayer)
+    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceId, JobId, lp)
 end)
 local Button = JoinSection:AddButton("Serverhop", function()
 	local TeleportService = game:GetService("TeleportService")
