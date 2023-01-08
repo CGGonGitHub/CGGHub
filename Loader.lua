3597a18fa40bf2eae1f8260b4247d272e34c3ee5
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-01-08 02:35:23 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -12,6 +12,7 @@ local Games = {
 	[5149673492] = "https://raw.githubusercontent.com/CGGonGitHub/CGGHub/main/Games/Build%20to%20survive%20the%20Bombs!.lua", -- Build To Survive The Bombs!
 	[11864536793] ="https://raw.githubusercontent.com/CGGonGitHub/CGGHub/main/Games/Dig%20to%20OHIO.lua", -- Dig to OHIO
 	[9498006165] = "https://raw.githubusercontent.com/CGGonGitHub/CGGHub/main/Games/Tapping%20Simulator.lua", -- Tapping Simulator
+	[6429911702] = "https://raw.githubusercontent.com/CGGonGitHub/CGGHub/main/Games/Master's%20Difficulty%20Chart%20Obby.lua", -- Master's Difficulty Chart Obby
 }
 
 if Games[game.PlaceId] then
